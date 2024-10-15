04888ed09e7893e6968d73f02eb84915b8a85f39
João Pedro Diniz <joopedrodiniz@gmail.com>
2024-10-15 16:02:57 -0300

    Add files via upload

---
+++ HOME.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+import streamlit as st
+
+
+st.set_page_config(
+        page_title= 'Programa momê',
+        page_icon='📑',
+       layout="wide",
+initial_sidebar_state="expanded")
+
+
+#---------------------------------------------#
+
+
+with st.container():
+    
+    st.markdown(
+    """ ## Consolidador de planilhas de deliverys"""  )
+
+
+    
+    st.markdown(
+    """ ### Utilize a barra lateral para acessar os programas, utilizar o programa conrespondente ao serviço de Delivery """  )
+    
+    
